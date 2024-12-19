ed82a82f7ff20f161d9e881b155bfefe056847e0
th1enq <thienchy3305@gmail.com>
2024-12-19 17:01:51 +0700

    update

--- DSA/Week-11/kmp.cpp
+++ DSA/Week-11/kmp.cpp
@@ -1,20 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string s;
+int n, m, q;
+long long d[111][111];
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-    cin >> s;
-    int n = s.size();
-    vector<int> pi(n, 0);
-    for(int i = 1; i < n; i++) {
-        int j = pi[i - 1];
-        while(j > 0 && s[i] != s[j]) j = pi[j - 1];
-        if(s[i] == s[j]) j++;
-        pi[i] = j;
+    cin >> n >> m >> q;
+
+    // gan oo cho moi cap dinh
+    memset(d, 0x3f, sizeof d);
+    for(int i = 1; i <= n; i++) d[i][i] = 0;
+
+    for(int i = 1; i <= m; i++) {
+        int u, v, w; cin >> u >> v >> w;
+        d[u][v] = d[v][u] = w;
     }
-    for(int i = 0; i < n; i++) cout << pi[i] << ' ';
+    for(int k = 1; k <= n; k++) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+            }
+        }
+    }
+    while(q--) {
+        int op, u, v; cin >> op >> u >> v;
+
+        cout << d[u][v] << "\n";
+    }
+
     return 0;
 }
